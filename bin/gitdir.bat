cb1d5b6308a6f3e3c7c3d7621926c18a0698a06a
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-07 13:42:33 -0800

    Update gitdir.bat

--- bin/gitdir.bat
+++ bin/gitdir.bat
@@ -1,2 +1,6 @@
 @echo OFF
-cd "%userprofile%\Google Drive\GitHub"
+
+@REM This script quicktly changes my directory to the GitHub Folder.
+
+IF EXIST "%userprofile%\Google Drive\GitHub" cd "%userprofile%\Google Drive\GitHub"
+IF NOT EXIST "%userprofile%\Google Drive\GitHub" echo "No Such Directory."
