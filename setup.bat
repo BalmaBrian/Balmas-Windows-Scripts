ca46f69c7110b02329281332a0425e6a516bcbae
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-07 08:25:33 -0800

    Update setup.bat

--- setup.bat
+++ setup.bat
@@ -1,5 +1,5 @@
 @echo OFF
 
-IF EXIST "%UserProfile%\Google Drive\GitHub\balmas-windows-scripts" setx path "%UserProfile%\Google Drive\GitHub\balmas-windows-scripts\bin"
+IF EXIST "%UserProfile%\Google Drive\GitHub\balmas-windows-scripts" setx path "%path%;%UserProfile%\Google Drive\GitHub\balmas-windows-scripts\bin"
 IF NOT EXIST "%UserProfile%\Google Drive" echo "~~ install google drive ~~"
 IF NOT EXIST "%UserProfile%\Google Drive\GitHub" echo "~~ install github desktop and set %UserProfile%\Google Drive\GitHub as repo directory in app ~~"
