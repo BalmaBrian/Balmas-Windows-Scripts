c0580582b9925e1912e55f115fb5b01a9ba5afcc
Brian Haro <44687168+BalmaBrian@users.noreply.github.com>
2020-12-11 08:18:03 -0800

    Delete spcvim.bat

--- bin/spcvim.bat
+++
@@ -1,6 +0,0 @@
-@echo OFF
-
-@REM This script runs a docker container that mounts the GitHub Directory
-@REM and prepares a space vim enviorment for simple Develpment Enviorment.
-
-docker run -it --rm --name "devenv" --mount type=bind,source="%userprofile%\Google Drive\GitHub",target=/home/spacevim/GitHub balmabrian/spacevim-base:ubuntu /bin/bash
